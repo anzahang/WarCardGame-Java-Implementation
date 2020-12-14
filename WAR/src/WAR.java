b3539a08954f0e74086f08eed96027badcf51ac2
andre <andre@192.168.0.20>
2020-12-14 17:41:15 -0500

    for multiple users

--- WAR/src/WAR.java
+++ WAR/src/WAR.java
@@ -1,40 +1,88 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class WAR {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args)throws Exception {
+		Scanner sc = new Scanner(System.in);
 		// number of players
 		int players = 2;
 
-		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
-
-		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
-
-		int n = suits.length * ranks.length;
-		int cardsPerPlayer = n/players;
+		ArrayList <Integer> deck = new ArrayList<Integer>();
 		
-		// initialize deck
-		String[] deck = new String[n];
-		for (int i = 0; i < ranks.length; i++) {
-			for (int j = 0; j < suits.length; j++) {
-				deck[suits.length * i + j] = ranks[i] + " of " + suits[j];
+		for(int i=0;i<52;i++) {
+			deck.add(i);
+		}
+		
+		int cardsPerPlayer = deck.size()/players;
+		
+		// shuffle
+		Random random = new Random(deck.size());
+		for (int i = 0; i < deck.size(); i++) {
+			int r = random.nextInt(deck.size());
+			int temp = deck.get(r);
+			deck.set(r, deck.get(i));
+			deck.set(i,temp);
+		}
+		
+		int cardSelect;
+		ArrayList<Integer>[] tiles = new ArrayList[players];
+		for(int i=0;i<players;i++) {
+			tiles[i]=new ArrayList<>();
+		}
+		int person = 0;
+		for(int deckIndex=0;deckIndex<deck.size();deckIndex++) {
+			tiles[person].add(deck.get(deckIndex));
+			person = (person+1) % players;
+		}
+		int personsInGame = players;
+		for(int i=0;personsInGame>1;i++) {
+			int winner=0;
+			int maxCard = Integer.MIN_VALUE;
+			ArrayList<Integer> drawnCards = new ArrayList<>();
+			for(int j=0;j<players;j++) {
+				System.out.print("You have " + tiles[j].size() + " cards. Please choose a card to be drawn: ");
+				cardSelect = sc.nextInt();
+				
+			
+				int cardNum = getCardNum(tiles[j].get(cardSelect));
+				drawnCards.add(tiles[j].get(cardSelect));
+				tiles[j].remove(cardSelect);
+				if(cardNum > maxCard) {
+					maxCard = cardNum;
+					winner = j;
+					
+				}
+			}
+			tiles[winner].addAll(drawnCards);
+			personsInGame = getPersonsInGame(tiles);
+			
+				
+		}
+		System.out.println("Game over, the winner is " + getWinner(tiles));
+		
+		
+	}
+
+	private static int getWinner(ArrayList<Integer>[] tiles)throws Exception {
+		int winner;
+		for(int i=0;i<tiles.length;i++) {
+			if(tiles[i].size() > 0) {
+				return i;
 			}
 		}
+		throw new Exception("Should not be here.");
+	}
 
-		// shuffle
-		for (int i = 0; i < n; i++) {
-			int r = (i + (int) (Math.random() * (n - i)))%n;
-			String temp = deck[r];
-			deck[r] = deck[i];
-			deck[i] = temp;
-		}
+	private static int getPersonsInGame(ArrayList<Integer>[] tiles) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-		// print shuffled deck
-		for (int i = 0; i < n; i++) {
-			System.out.println(deck[i]);
-			if (i % cardsPerPlayer == cardsPerPlayer - 1)
-				System.out.println();
-		}
+	private static int getCardNum(Integer cardID) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
